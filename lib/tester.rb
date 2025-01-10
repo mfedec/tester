8d8ba33e3ed6bc7ba7d05ee9aae7dd448828211a
Martin Fedec <martin@drugbank.com>
2025-01-09 21:21:36 -0800

    adding gem template

---
+++ lib/tester.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tester/version"
+
+module Tester
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tester/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tester
+  VERSION = "0.1.0"
+end
