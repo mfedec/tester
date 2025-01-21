d18e09136d7a796c5dcbd02bcdcb7d732e91606d
Martin Fedec <martin@drugbank.com>
2025-01-20 21:37:32 -0800

    Dont need this user created for this test

--- spec/integration/docker/source/ubuntu_setup.sh
+++ spec/integration/docker/source/ubuntu_setup.sh
@@ -5,15 +5,6 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 apt -y update
 
-# Create `tester` user that can sudo without a password
-apt-get -y install sudo
-adduser --disabled-password tester < /dev/null
-echo "tester:topsecret" | chpasswd
-echo "tester ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-mkdir /home/tester/.ssh && ln -s /shared/ssh/id_rsa.pub /home/tester/.ssh/authorized_keys
-chown -R tester: /home/tester/.ssh
-
 # Install and configure sshd
 apt-get -y install openssh-server
 {
